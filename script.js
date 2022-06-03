873f7a8733651212c56f02738bf7e5298e3fd2a0
tdNoobie <thomas.danielse@gmx.de>
2022-06-03 11:33:00 +0200

    renderHTML onload, render Table HTML onclick

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let responseAsJson
+
 function init() {
     loadRockets();
 /*     loadLandpads();
@@ -6,33 +8,52 @@ function init() {
 async function loadRockets() {
     let url = `https://api.spacexdata.com/v4/rockets`;
     let response = await fetch(url);
-    let responseAsJson = await response.json();
+    responseAsJson = await response.json();
     console.log('Rockets:', responseAsJson);
-    showData(responseAsJson);
+    renderRockets(responseAsJson);
 }
 
 
 /*https://api.spacexdata.com/v4/landpads */
 
-function showData(rockets) {
-
-    let html = document.getElementById('rocketContainer');
+function renderRockets(rockets) {
+    let RocketsHTML = document.getElementById('rocketContainer');
     for (let i = 0; i < rockets.length; i++) {
         const rocket = rockets[i];
-        html.innerHTML += generateRocketsHTML(rocket, i);
+        RocketsHTML.innerHTML += generateRocketsHTML(rocket, i);
     }
 
 }
 
 
-function openData(i) {
+function renderData(index) {
+    addBlurr();
+    let dataHTML = document.getElementById('rocketContainer');
+        const data = responseAsJson[index];
+        
+        dataHTML.innerHTML += generateDataHTML(data, index);
+        renderPayloadWeights(index);
 
+        function renderPayloadWeights(j) {
+            for (let i = 0; i < responseAsJson[j]['payload_weights'].length; i++) {
+                const element = responseAsJson[j][i];
+             dataHTML.innerHTML += `${element['id']}`;   
+            }
+        }
 }
 
 
+
+function addBlurr() {
+    let element = document.querySelectorAll('[id^="rocketCards"]');
+    for (let i = 0; i < element.length; i++) {
+        element[i].style = 'filter: blur(4px)';        
+    }
+}
+
 function generateRocketsHTML(rocket, i) {
     return /* html */ `
-                       <div class="card bg-transparent">
+                       <div id="rocketCards" class="card bg-transparent">
                             <div class="card-input">
                             <h5 class="card-title">${rocket['name']}</h5>
                             <img src="${rocket['flickr_images'][0]}" class="card-img-top" alt="...">
@@ -41,35 +62,55 @@ function generateRocketsHTML(rocket, i) {
                             </div>
                             </div>
                             <div class="card-btn">
-                                <button onclick="openData(i)" class="btn btn-primary">Open Data</button>
-                                <button onclick="openImages(i)" class="btn btn-primary">Open Images</button>
+                                <button onclick="renderData(${i})" class="btn btn-primary">Open Data</button>
+                                <button onclick="renderImages(${i})" class="btn btn-primary">Open Images</button>
                             </div>                        
                         </div> 
                         `;
 }
 
 
-
-//function generateRocketsHTML(rocket, i) {
-//    return /* html */ `
-//    <div class="rocket-table">
-//        <img src="${rocket['flickr_images'][0]}" alt="">
-//        <table class="table">
-//            <tr>
-//                <th>Name</th>
-//                <td>${rocket['name']}</td>
-//            </tr>
-//            <tr>
-//                <th>Stages</th>
-//                <td>${rocket['stages']}</td>
-//            </tr>
-//            <tr>
-//                <th>Boosters</th>
-//                <td>${rocket['boosters']}</td>
-//            </tr>
-//        </table>
-//        </div>
-//
-//                    `;
-//}
-
+function generateDataHTML(data, index) {
+    return /* html */ `
+    <img src="./img/close.png" alt="" type="button" class="btn-close">
+    <div class="rocket-table">
+        <table class="table table-dark table-striped table-borderless table-hover">
+            <tr>
+                <th>Name</th>
+                <td>${data['name']}</td>
+            </tr>
+            <tr>
+                <th>Stages</th>
+                <td>${data['stages']}</td>
+            </tr>
+            <tr>
+                <th>Boosters</th>
+                <td>${data['boosters']}</td>
+            </tr>            
+            <tr>
+                <th>Diameter(m)</th>
+                <td>${data['diameter']['meters']}</td>
+            </tr>
+            <tr>
+                <th>Heigth(m)</th>
+                <td>${data['height']['meters']}</td>
+            </tr>
+            <tr>
+                <th>Mass(kg)</th>
+                <td>${data['mass']['kg']}</td>
+            </tr>
+            <tr>
+                <th>Payload Weight(kg)</th>
+                <tr>
+                    <th>${data['payload_weights']}</th>
+                    <td>${data['payload_weights'][0]['kg']}</td>
+                </tr>
+            </tr>
+            <tr>
+                <th>Boosters</th>
+                <td>${data['boosters']}</td>
+            </tr>
+        </table>
+        </div>
+        `;
+}
